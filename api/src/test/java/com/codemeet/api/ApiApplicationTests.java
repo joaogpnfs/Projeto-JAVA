3afc985d6bba3315a75a6e059190da86976204d6
joaogpnfs <jgpnfeitosa57@gmail.com>
2025-01-07 11:42:12 -0300

    feat: first commit

---
+++ api/src/test/java/com/codemeet/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemeet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
